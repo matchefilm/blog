5eff8b77b0ba5b9839157912f3ee4467c56cb0af
matchefilm <mywx172@qq.com>
2024-11-25 16:08:10 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navbar = document.querySelector('.navbar');
+
+    menuToggle.addEventListener('click', () => {
+        navbar.classList.toggle('active');
+    });
+});
